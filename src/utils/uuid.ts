b9bdb3d56708b3aac5a79e259f25b08d1cb8328b
Tynan Daly <tynan.s.daly@gmail.com>
2024-02-16 22:44:47 -0500

    generate uuids

---
+++ src/utils/uuid.ts
@@ -0,0 +1,18 @@
+import { randomBytes } from "crypto";
+
+export function generateUUID() {
+  const bytes = randomBytes(16);
+  bytes[6] = (bytes[6] & 0x0f) | 0x40; // Version 4 UUID
+  bytes[8] = (bytes[8] & 0x3f) | 0x80; // Variant 10xx for UUID
+
+  const uuid = `${bytes.toString("hex", 0, 4)}-${bytes.toString(
+    "hex",
+    4,
+    6
+  )}-${bytes.toString("hex", 6, 8)}-${bytes.toString(
+    "hex",
+    8,
+    10
+  )}-${bytes.toString("hex", 10, 16)}`;
+  return uuid;
+}
---
+++ tests/utils/utils.test.ts
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "@jest/globals";
+import { generateUUID } from "../../src/utils/uuid";
+
+test("generateUUID", () => {
+  const uuid = generateUUID();
+  expect(uuid).toMatch(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+  );
+});
