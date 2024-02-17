fe939cb2b6be72fe94ade6f876ce58211acde6d6
Tynan Daly <tynan.s.daly@gmail.com>
2024-02-16 23:40:35 -0500

    hdr config type

--- src/memories/memory.ts
+++ src/memories/memory.ts
@@ -1,4 +1,11 @@
+import { z } from "zod";
 import { ObjectiveState } from "../types/browser.types";
+
+export const HDRConfig = z.object({
+  apiKey: z.string().length(1),
+});
+
+export type HDRConfig = z.infer<typeof HDRConfig>;
 
 // fill out
 export async function remember(objectiveState: ObjectiveState) {}
