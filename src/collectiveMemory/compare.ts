db0872c7f77a3e676f29e5ac86d6bab4df05a499
Tynan Daly <tynan.s.daly@gmail.com>
2024-04-16 15:58:18 -0400

    start of memory comparison

---
+++ src/collectiveMemory/compare.ts
@@ -0,0 +1,21 @@
+import { ObjectiveState } from "../types/browser";
+import { BrowserAction } from "../types/browser/actions.types";
+import { Memory } from "../types/memory.types";
+
+// export function compareMemories(
+//   command: BrowserAction,
+//   currentState: ObjectiveState,
+//   memory: Memory
+// ): boolean {
+//   return false;
+// }
+
+// export function diffMemory(
+//   commands: BrowserAction[],
+//   currentState: ObjectiveState,
+//   memory: Memory
+// ): boolean {
+//   if (!memory || !currentState) {
+//     return false;
+//   }
+// }
