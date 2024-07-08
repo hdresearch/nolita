a2c734dd85a4866ee140c7c09e1aebb73da0adda
Tynan Daly <tynan.s.daly@gmail.com>
2024-07-08 13:06:51 -0400

    doc strings on generator functions

--- src/agent/generators/generateObject.ts
+++ src/agent/generators/generateObject.ts
@@ -5,6 +5,21 @@ import { generateObjectProvider } from "./generateObjectProvider";
 import { LlamaModel } from "node-llama-cpp";
 import { ObjectGeneratorOptions } from "./types";
 
+/**
+ * Wrapper function to generate an object using either the local model or a provider.
+ * @param client The client to use
+ * @param messages The messages to use
+ * @param options The options for the object generator
+ * @param options.schema The schema for the object
+ * @param options.name The name of the object (used in the tool call)
+ * @param options.model The model to use for the object generation
+ * @param options.objectMode The object mode to use for the object generation (default: TOOLS)
+ * @param options.maxRetries The maximum number of retries (default: 3)
+ * @param options.maxTokens The maximum number of tokens (default: 1000)
+ * @param options.temperature The temperature for the model (default: 0)
+ * @param options.topP The topP for the model (default: 1)
+ * @returns The generated object
+ */
 export async function generateObject<T extends z.ZodSchema<any>>(
   client: any,
   messages: any,
--- src/agent/generators/generateObjectProvider.ts
+++ src/agent/generators/generateObjectProvider.ts
@@ -3,6 +3,20 @@ import Instructor from "@instructor-ai/instructor";
 
 import { ObjectGeneratorOptions } from "./types";
 
+/**
+ *
+ * @param client The LLM client
+ * @param messages The message array to send to the model
+ * @param options The options for the object generator
+ * @param options.schema The return schema for the object
+ * @param options.name The name of the object (used in the tool call)
+ * @param options.model The model to use for the object generation
+ * @param options.objectMode The object mode to use for the object generation (default: TOOLS)
+ * @param options.maxRetries The maximum number of retries (default: 3)
+ * @param options.maxTokens The maximum number of tokens (default: 1000)
+ * @param options.temperature The temperature for the model (default: 0)
+ * @returns
+ */
 export async function generateObjectProvider<T extends z.ZodSchema<any>>(
   client: any,
   messages: any,
