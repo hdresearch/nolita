6bde7190952632902a253dedb770a3f13e62449b
Matilde Park <matilde.park@pm.me>
2024-07-16 15:21:56 -0400

    Revert "types/browser: extend z"
    This reverts commit 87b1c72cc059879894ac1ab30aceb95aa9e6370a.

--- src/server/pages/do.ts
+++ src/server/pages/do.ts
@@ -1,4 +1,5 @@
-import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
+import { z } from "@hono/zod-openapi";
 
 import { BROWSERS } from "../browser/launch";
 import { PageParamsSchema } from "../schemas";
