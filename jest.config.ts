da95786c90e0a154b574fa4ea9227129f09e92bb
Matilde Park <matilde.park@pm.me>
2024-08-14 14:41:50 -0400

    jest: fix resolver

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,12 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
-  testTimeout: 20000
+  testTimeout: 20000,
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
 
 export default config;
